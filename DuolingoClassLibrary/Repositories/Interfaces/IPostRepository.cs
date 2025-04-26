8177693d776d7b4ac071830b7a4278e5fed18930
Vlad <vlad.cenuse@gmail.com>
2025-04-26 13:27:07 +0300

    API routes work

--- DuolingoClassLibrary/Repositories/Interfaces/IPostRepository.cs
+++ DuolingoClassLibrary/Repositories/Interfaces/IPostRepository.cs
@@ -5,5 +5,11 @@ namespace DuolingoClassLibrary.Repositories.Interfaces
     public interface IPostRepository
     {
         public Task<List<Post>> GetPosts();
+
+        public Task<int> CreatePost(Post post);
+
+        public Task DeletePost(int id);
+
+        public Task UpdatePost(Post post);
     }
 }

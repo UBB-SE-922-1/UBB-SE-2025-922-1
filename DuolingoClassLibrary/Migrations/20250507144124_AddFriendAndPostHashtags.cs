48baa8a61469edbab717f6589a3248b75e359dc6
Vlad <146939321+vladcenuse@users.noreply.github.com>
2025-05-08 13:01:13 +0300

    ASP.net v2 (#59)
    * ASP.NET model. Should work Hopefully
    
    * ASP.net setup attempt

---
+++ DuolingoClassLibrary/Migrations/20250507144124_AddFriendAndPostHashtags.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DuolingoClassLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFriendAndPostHashtags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Hashtags",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Hashtags");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Comments");
+
+            migrationBuilder.CreateTable(
+                name: "Friends",
+                columns: table => new
+                {
+                    FriendshipId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId1 = table.Column<int>(type: "int", nullable: false),
+                    UserId2 = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Friends", x => x.FriendshipId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostHashtags",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "int", nullable: false),
+                    HashtagId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostHashtags", x => new { x.PostId, x.HashtagId });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Friends");
+
+            migrationBuilder.DropTable(
+                name: "PostHashtags");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Date",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Hashtags",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Hashtags",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
